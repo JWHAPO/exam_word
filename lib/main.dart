ede05c2c3c655b0b38de426e2eef783b8be3ede0
JWHAPO <kim2101714@gmail.com>
2019-12-27 16:10:01 +0900

    git init

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:exam/result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '다정스쿨',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '다정스쿨'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
+  final int changeTime = 1000;
+  bool isStart = false;
+  bool isEnd = false;
+  String stage = '';
+  String word='';
+  double duration = 0.0;
+  int point = 0;
+  int index = 0;
+  final TextEditingController _answerController =TextEditingController();
+  List<String> words = [
+    'Apple', 'Banana', 'Cherry', 'Dragon'
+  ];
+  List<List<String>> answers = [
+    ['사과'],
+    ['바나나','버내너','버네너'],
+    ['체리','췌리'],
+    ['용']
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void firstStart(){
+    setState(() {
+      isStart = true;
+      isEnd = false;
+      int tempIndex = index ~/2;
+      if(tempIndex >= words.length) {
+        index = 0;
+        tempIndex = index ~/2;
+      }else{
+        stage = '${tempIndex+1} / ${words.length}';
+        word = words[tempIndex];
+        duration = duration==1.0?0.0:1.0;
+        index ++;
+        changeWord();
+      }
+    });
+  }
+
+  void changeWord(){
+    Future.delayed(Duration(milliseconds: changeTime), () {
+      setState(() {
+        int tempIndex = index ~/2;
+        if(tempIndex >= words.length) {
+          index = 0;
+          tempIndex = index ~/2;
+          isEnd = true;
+          isStart = false;
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context)=>ResultPage(point, words.length)));
+
+        }else{
+          stage = '${tempIndex+1} / ${words.length}';
+          word = words[tempIndex];
+
+          if(index%2 ==0){
+            addPoint(tempIndex);
+            _answerController.text = '';
+          }
+
+          duration = duration==1.0?0.0:1.0;
+          index ++;
+          changeWord();
+        }
+      });
+    });
+  }
+
+  void addPoint(int index){
+    if(answers[index-1].contains(_answerController.value.text.trim())){
+      point++;
+    }
+    print('point$point');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('다정스쿨'),
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(stage, style: TextStyle(fontSize: 16.0, color: Colors.blueAccent),),
+              isStart?
+              Column(
+                children: <Widget>[
+                  AnimatedOpacity(
+                    opacity: duration,
+                    child: Text(word, style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),),
+                    duration: Duration(milliseconds: changeTime),
+                  ),
+                  TextField(
+                    controller: _answerController,
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                    decoration: InputDecoration(
+                      labelText: '정답',
+                      labelStyle: TextStyle(color: Colors.black, fontSize: 14),
+                    ),
+                  )
+                ],
+              ):Column(
+                children: <Widget>[
+                  Text('이번 주 단어 시험', style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 10.0,),
+                  RaisedButton(
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.black54)),
+                    onPressed: () {
+                      firstStart();
+                    },
+                    color: Colors.white,
+                    textColor: Colors.black54,
+                    child: Text("시작",style: TextStyle(fontSize: 18)),
+                  ),
+                ],
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/result.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:exam/main.dart';
+
+class ResultPage extends StatelessWidget {
+  final int point;
+  final int total;
+
+  ResultPage(this.point, this.total);
+
+  @override
+  Widget build(BuildContext context) {
+    int result = ((point / total) * 100).toInt();
+
+    String text1;
+    String text2;
+    String imgPath;
+
+    text1 = '$result 점';
+    if(result>50){
+      text2 = '형아, 누나들 잘했어요';
+      imgPath = 'assets/img/good.jpg';
+    }else{
+      text2 = '형아, 누나들 더 공부하세요';
+      imgPath = 'assets/img/bad.jpg';
+    }
+
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Image.asset(imgPath,width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height,fit: BoxFit.contain,),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.68, bottom: MediaQuery.of(context).size.height*0.04),
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular( 16.0),),
+                  border: Border.all(width: 2, color: Colors.black)
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Text(text1,style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.red)),
+                    Text(text2,style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black87)),
+                    RaisedButton(
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.black87)),
+                      onPressed: () {
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context)=>MyHomePage()));
+                      },
+                      color: Colors.white,
+                      textColor: Colors.black87,
+                      child: Text("다시하기",style: TextStyle(fontSize: 18)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
