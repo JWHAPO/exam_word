23d81293f1254fe80981539e1c8d374d5fd0f4d8
JWHAPO <kim2101714@gmail.com>
2020-01-05 00:39:29 +0900

    add widget in signup

--- lib/ui/signup.dart
+++ lib/ui/signup.dart
@@ -9,32 +9,95 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        FocusScope.of(context).requestFocus(FocusNode());
-      },
-      child: GestureDetector(
         onTap: (){
           FocusScope.of(context).requestFocus(FocusNode());
         },
         child: Scaffold(
           body: Container(
-            padding: EdgeInsets.only(left: 80.0, right: 80.0),
+            padding: EdgeInsets.only(left: 80.0,),
             child: Column(
               children: <Widget>[
                 SizedBox(
                   height: 50.0,
                 ),
-                Stack(
-                  children: <Widget>[
-                    //배경 이미지
-                    //회원가입 글자
-                  ],
+                Container(
+                  padding: EdgeInsets.only(right: 50.0),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height* 0.2,
+                  child: Stack(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Icon(Icons.spellcheck,color: Color(0x805AAEFF),size: 160.0,)
+                      ),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text('회원가입',style: TextStyle(fontFamily: 'NanumGothic-ExtraBold', fontSize: 26.0, color: Colors.black),)
+                      )
+                    ],
+                  ),
                 ),
                 SizedBox(
-                  height: 50.0,
+                  height: 20.0,
                 ),
                 TextField(
+                  controller: _idTextController,
+                  style: TextStyle(color: Colors.black, fontFamily: 'NanumGothic-Regular', fontSize: 16.0),
+                  decoration: InputDecoration(
+                    labelText: 'ID',
+                    hintText: 'ID를 입력하세요.',
+                    labelStyle: TextStyle(color: Colors.grey, fontFamily: 'NanumGothic-Regular', fontSize: 16.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  controller: _pwTextController,
+                  style: TextStyle(color: Colors.black, fontFamily: 'NanumGothic-Regular', fontSize: 16.0),
+                  decoration: InputDecoration(
+                    labelText: '비밀번호',
+                    hintText: '비밀번호를 입력하세요.',
+                    labelStyle: TextStyle(color: Colors.grey, fontFamily: 'NanumGothic-Regular', fontSize: 16.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  controller: _pwConfirmTextController,
+                  style: TextStyle(color: Colors.black, fontFamily: 'NanumGothic-Regular', fontSize: 16.0),
+                  decoration: InputDecoration(
+                    labelText: '비밀번호 확인',
+                    hintText: '위 비밀번호와 같게 입력하세요.',
+                    labelStyle: TextStyle(color: Colors.grey, fontFamily: 'NanumGothic-Regular', fontSize: 16.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextField(
+                  controller: _nameConfirmTextController,
+                  style: TextStyle(color: Colors.black, fontFamily: 'NanumGothic-Regular', fontSize: 16.0),
+                  decoration: InputDecoration(
+                    labelText: '이름',
+                    hintText: '이름을 입력하세요.',
+                    labelStyle: TextStyle(color: Colors.grey, fontFamily: 'NanumGothic-Regular', fontSize: 16.0),
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: RaisedButton(
+                    onPressed: (){
 
+                    },
+                    color: Theme.of(context).accentColor,
+                    child: Text('가입하기', style: TextStyle(fontFamily: 'NanumGothic-ExtraBold', fontSize: 14.0, color: Colors.white), ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20.0), topLeft: Radius.circular(20.0))
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -45,7 +108,6 @@ class SignUpPage extends StatelessWidget {
             //반
           ),
         ),
-      ),
-    );
+      );
   }
 }
